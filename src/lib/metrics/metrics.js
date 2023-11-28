0b8150bcc12778be527ad1fefc992db3728865ba
João André Marques <joaoandremarques@gmail.com>
2023-11-28 23:06:18 +0000

    created first metrics variables

---
+++ src/lib/metrics/metrics.js
@@ -0,0 +1,6 @@
+import { returnUrl } from "../parser/xml-parser";
+
+export const MaximumHeartRateBpm = await returnUrl("MaximumHeartRateBpm");
+export const HeartRateBpm = await returnUrl("HeartRateBpm");
+export const AverageHeartRateBpm = await returnUrl("AverageHeartRateBpm");
+export const Position = await returnUrl("Position");
