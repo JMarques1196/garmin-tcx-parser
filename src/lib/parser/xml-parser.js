d4f529e5bf081651fdcdf99493a9e5ee0fb6b8ec
João André Marques <joaoandremarques@gmail.com>
2023-12-10 23:17:13 +0000

    config file

--- src/lib/parser/xml-parser.js
+++ src/lib/parser/xml-parser.js
@@ -1,6 +1,6 @@
 import { url } from "../../config/config";
 
-export async function fetchUrl() {
+async function fetchUrl() {
   const response = await fetch(url); // Fetch tcx file from provided url
   const data = await response.text();
 
