041d708c81c32202523d713a315be1c54f3f279d
João André Marques <joaoandremarques@gmail.com>
2024-01-19 16:10:53 +0000

    feat: new metrics

--- src/lib/metrics/metrics.js
+++ src/lib/metrics/metrics.js
@@ -3,10 +3,12 @@ import { returnMetric } from "../parser/xml-parser";
 export const MaximumHeartRateBpm = await returnMetric("MaximumHeartRateBpm");
 export const HeartRateBpm = await returnMetric("HeartRateBpm");
 export const AverageHeartRateBpm = await returnMetric("AverageHeartRateBpm");
-export const Position = await returnMetric("Position");
+export const LatitudeDegrees = await returnMetric("LatitudeDegrees");
+export const LongitudeDegrees = await returnMetric("LongitudeDegrees");
+export const Cadence = await returnMetric("Cadence");
 export const TotalTimeSeconds = await returnMetric("TotalTimeSeconds");
 export const DistanceMeters = await returnMetric("DistanceMeters");
 export const AltitudeMeters = await returnMetric("AltitudeMeters");
 export const Calories = await returnMetric("Calories");
 export const MaximumSpeed = await returnMetric("MaximumSpeed");
-export const Time = await returnMetric("Time");
+
